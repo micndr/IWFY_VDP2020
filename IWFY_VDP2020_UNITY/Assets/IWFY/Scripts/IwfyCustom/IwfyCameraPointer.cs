f8d426f8eaa79a8f91c52b86e4e803746ace0cf4
magiwanders <magi.wanders@gmail.com>
2020-11-08 22:28:30 +0100

    Merging of experiments

---
+++ IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/IwfyCustom/IwfyCameraPointer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IwfyCameraPointer : MonoBehaviour
+{
+    private const float _maxDistance = 10;
+    private GameObject _gazedAtObject = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, _maxDistance))
+        {
+            // GameObject detected in front of the camera.
+            if (_gazedAtObject != hit.transform.gameObject)
+            {
+                // New GameObject.
+                _gazedAtObject?.SendMessage("OnPointerExit");
+                _gazedAtObject = hit.transform.gameObject;
+                _gazedAtObject.SendMessage("OnPointerEnter");
+            }
+        }
+        else
+        {
+            // No GameObject detected in front of the camera.
+            _gazedAtObject?.SendMessage("OnPointerExit");
+            _gazedAtObject = null;
+        }
+        
+        if (Input.GetMouseButtonDown(0) || Google.XR.Cardboard.Api.IsTriggerPressed)
+        {
+            _gazedAtObject?.SendMessage("OnPointerClick");
+        }
+    }
+}
---
+++ IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/IwfyCustom/IwfyReticleAnimator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IwfyReticleAnimator : MonoBehaviour
+{
+    public Animator animator;
+    void OnClickableObjectEnter()
+    {
+        Debug.Log("[CustomReticlePointer.cs] ReticlePointerEntered a Clickable Object");
+        animator.SetBool("PointingClickableObject", true);
+    }
+
+    void OnClickableObjectExit()
+    {
+        Debug.Log("[CustomReticlePointer.cs] ReticlePointerExited a Clickable Object");
+        animator.SetBool("PointingClickableObject", false);
+    }
+}
