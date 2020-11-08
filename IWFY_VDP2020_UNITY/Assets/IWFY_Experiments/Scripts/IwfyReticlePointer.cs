d16c1cbd92519bf38eaac7a4ace977ae667c7973
magiwanders <magi.wanders@gmail.com>
2020-11-08 19:11:54 +0100

    Working reticle pointer in Unity - Not yet compiled

--- IWFY_VDP2020_UNITY/Assets/IWFY_Experiments/Scripts/IwfyReticlePointer.cs
+++ IWFY_VDP2020_UNITY/Assets/IWFY_Experiments/Scripts/IwfyReticlePointer.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 
 public class IwfyReticlePointer : MonoBehaviour
 {
+    public Animator animator;
     void OnClickableObjectEnter()
     {
         Debug.Log("[CustomReticlePointer.cs] ReticlePointerEntered a Clickable Object");
+        animator.SetBool("PointingClickableObject", true);
     }
 
     void OnClickableObjectExit()
     {
         Debug.Log("[CustomReticlePointer.cs] ReticlePointerExited a Clickable Object");
+        animator.SetBool("PointingClickableObject", false);
     }
 }
