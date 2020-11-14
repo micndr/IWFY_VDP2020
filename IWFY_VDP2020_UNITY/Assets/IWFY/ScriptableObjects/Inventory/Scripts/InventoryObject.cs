f19667af210e1f10030e6c7234b2c0bb67d69f30
HomelessNoob <michele.zhu@mail.polimi.it>
2020-11-14 21:12:40 +0100

    Inventory v1

---
+++ IWFY_VDP2020_UNITY/Assets/IWFY/ScriptableObjects/Inventory/Scripts/InventoryObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory System/Inventory")]
+public class InventoryObject : ScriptableObject
+{
+    public List<InventorySlot> Container = new List<InventorySlot>(); //Storing items in a list
+
+    public void AddItem(ItemObject _item, int _amount)
+    {
+        for (int i = 0; i < Container.Count; i++)
+        {
+            if (Container[i].item == _item)
+            {
+                Container[i].AddAmount(_amount);
+                return;
+            }
+        }
+        Container.Add(new InventorySlot(_item,_amount));
+    }
+
+    public void RemoveItem(InventorySlot _item)
+    {
+        Container.Remove(_item);
+    }
+}
+
+[System.Serializable]
+public class InventorySlot
+{
+    public ItemObject item;
+    public int amount;
+
+    public InventorySlot(ItemObject _item, int _amount)
+    {
+        item = _item;
+        amount = _amount;
+    }
+
+    public void AddAmount(int value)
+    {
+        amount += value;
+    }
+}
