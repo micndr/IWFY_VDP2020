c4c7d8c38e513792bee650916e4358fdef96dd38
jacopo grandi <jak.sk8@hotmail.it>
2020-11-15 10:23:12 +0100

    rain and doc

---
+++ IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/experiments/FxRain.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FxRain : MonoBehaviour {
+
+    ParticleSystem particles;
+    ParticleSystem.ShapeModule shape;
+
+    GameObject player;
+
+    public float rainHeight = 180;
+
+    void Start() {
+        particles = transform.Find("rainParticles").GetComponent<ParticleSystem>();
+        shape = particles.shape;
+
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update() {
+        Quaternion rot = Quaternion.FromToRotation(
+            Vector3.up,
+            player.transform.position.normalized);
+
+        Vector3 pos = rot * new Vector3(0, rainHeight, 0);
+        shape.position = new Vector3(pos.x, -pos.z, pos.y);
+
+        Vector3 rotDeg = rot.eulerAngles;
+        shape.rotation = new Vector3(
+            rotDeg.x,
+            -rotDeg.z,
+            rotDeg.y);
+    }
+}
