419592376713d85129f32be39bc1c0f9602cf0ab
micndr <micndr@gmail.com>
2020-11-18 08:59:41 +0100

    added play videos feature

---
+++ IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/videoController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class videoController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _videoPlayer;
+    [SerializeField] private int _timeToStop;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _videoPlayer.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("prova"))
+        {
+            _videoPlayer.SetActive(true);
+            Destroy(_videoPlayer, _timeToStop);
+        }
+    }
+}
