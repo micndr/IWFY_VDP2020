93138f6279f55ed9530b5d54e196b33d2936d8e4
jacopo grandi <jak.sk8@hotmail.it>
2020-11-23 19:05:04 +0100

    global quests

---
+++ IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/GlobalState.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GlobalState : MonoBehaviour { 
+    public static GlobalState Instance;
+
+    public Transform playerPos;
+    public List<string> completedQuests = new List<string>();
+
+    void Awake() {
+        if (Instance == null) {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        } else if (Instance != this) {
+            Destroy(gameObject);
+        }
+        SceneManager.sceneLoaded += this.OnLoadCallback;
+    }
+
+    public void AddQuest (QuestMain qm) {
+        completedQuests.Add(qm.questName);
+    }
+
+    void OnLoadCallback(Scene scene, LoadSceneMode sceneMode) { 
+        var qms = FindObjectsOfType<QuestMain>();
+        foreach (QuestMain qm in qms) {
+            if (completedQuests.Contains(qm.questName)) {
+                // TODO: change quest completion
+                qm.state = qm.stateNames.Length - 1;
+                qm.completed = true;
+            }
+        }
+    }
+}
