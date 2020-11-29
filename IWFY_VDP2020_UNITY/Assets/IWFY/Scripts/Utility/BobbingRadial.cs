10d344483a65856ea73970e993e607ad8f167a80
jacopo grandi <jak.sk8@hotmail.it>
2020-11-29 18:52:47 +0100

    flowers

---
+++ IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/Utility/BobbingRadial.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BobbingRadial : MonoBehaviour {
+
+    public float amplitude = 0.001f;
+    public float speed = 1f;
+
+    void Update() {
+        transform.position += transform.position.normalized * Mathf.Sin(Time.time * speed) * amplitude;
+    }
+}
