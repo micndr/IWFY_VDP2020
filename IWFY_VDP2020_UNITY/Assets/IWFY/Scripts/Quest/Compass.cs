8c562913abb016ba2c4698fee089f7c58a07e690
jacopo grandi <jak.sk8@hotmail.it>
2020-11-29 11:28:27 +0100

    compass

---
+++ IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/Quest/Compass.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour {
+
+    public GameObject arrow;
+    public Transform target;
+
+
+    void Update() {
+        if (target) {
+            arrow.gameObject.SetActive(true);
+            Vector3 toTarget = target.position - transform.position;
+            arrow.transform.rotation = Quaternion.LookRotation(toTarget.normalized, transform.position.normalized);
+            arrow.transform.localEulerAngles = new Vector3(0, arrow.transform.localEulerAngles.y, 0);
+        } else {
+            arrow.gameObject.SetActive(false);
+        }
+    }
+}
