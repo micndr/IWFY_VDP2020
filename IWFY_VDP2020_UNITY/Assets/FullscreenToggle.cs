a824cd379c2548225325576f0e24079d9fec6998
jacopo grandi <jak.sk8@hotmail.it>
2020-12-08 13:26:54 +0100

    res

---
+++ IWFY_VDP2020_UNITY/Assets/FullscreenToggle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FullscreenToggle : MonoBehaviour {
+
+    Toggle toggle;
+
+    private void Awake() {
+        toggle = GetComponent<Toggle>();
+    }
+
+    public void onValueChanged() {
+        Screen.SetResolution(Screen.width, Screen.height, toggle.isOn);
+    }
+}
---
+++ IWFY_VDP2020_UNITY/Assets/ResDropdown.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResDropdown : MonoBehaviour {
+
+    Dropdown drop;
+    List<Resolution> ress = new List<Resolution>();
+
+    void Awake() {
+        foreach (Resolution res in Screen.resolutions) {
+            if (res.width >= 800) {
+                ress.Add(res);
+            }
+        }
+
+        drop = GetComponent<Dropdown>();
+        foreach (Resolution res in ress) {
+            drop.options.Add(new Dropdown.OptionData(res.ToString()));
+        }
+    }
+
+    public void OnValueChanged () {
+        Resolution res = ress[drop.value];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
+    }
+
+    public void Update() {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            Screen.SetResolution(1920, 1080, Screen.fullScreen);
+        }
+    }
+}
