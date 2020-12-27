49d7e54127125490b548a2ac6fcfb3933cb0b443
micndr <micndr@gmail.com>
2020-12-28 00:57:18 +0100

    added bridge animation on world 3

---
+++ IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/World3/momentanea.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class momentanea : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            animator.SetBool("attivo", true);
+            Debug.Log("è vero");
+        }
+    }
+}
