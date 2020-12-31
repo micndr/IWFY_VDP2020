cda41188b96aeb2b8982680e953e86f56d7cb5a8
magiwanders <magi.wanders@gmail.com>
2020-12-31 12:55:54 +0100

    Working scrigno controller prototype.

---
+++ IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/World1/GhieraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhieraController : IwfyClickableObjectNoPopup
+{
+    // The number must be the index from zero starting from the innermost ghiera.
+    public int ghieraNumber;
+    [SerializeField] private GameObject _scrigno;
+
+    public override void OnPointerClick()
+    {
+        base.OnPointerClick();
+        _scrigno.SendMessage("GhieraClicked", ghieraNumber);
+    }
+}
