945f808ca2613e5d1293caa9efc4f131779ca115
micndr <micndr@gmail.com>
2020-12-31 03:49:49 +0100

    minor bugfix on mirror + note appear system alpha alpha

---
+++ IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/openNote.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class openNote : MonoBehaviour
+{
+    [SerializeField] private Image note;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            note.enabled = true;
+            
+        }
+            
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.U))
+            if (note.enabled)
+                note.enabled = false;
+    }
+}
