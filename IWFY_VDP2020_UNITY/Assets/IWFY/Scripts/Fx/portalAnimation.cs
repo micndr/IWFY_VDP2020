44a917e31e6fef246480799873dfc0ffedd397ec
jacopo grandi <jak.sk8@hotmail.it>
2021-01-01 23:13:56 +0100

    control anim portal parameters

--- IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/Fx/portalAnimation.cs
+++ IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/Fx/portalAnimation.cs
@@ -11,6 +11,12 @@ public class portalAnimation : MonoBehaviour {
     float timer;
     float duration;
 
+    public int numOfParticles = 6;
+    public float timeBetweenPulses = 1;
+    public float speedForwardMovement = 1;
+    public float speedScaling = 1;
+    public float speedTransparency = 1;
+
     void Start() {
         
     }
@@ -20,11 +26,11 @@ public class portalAnimation : MonoBehaviour {
             Destroy(child);
         }
         portalFxs.Clear();
-        for (int i=0; i<6; i++) {
+        for (int i=0; i< numOfParticles; i++) {
             GameObject obj = GameObject.Instantiate(portalFx);
             obj.transform.position = transform.position;
             obj.transform.rotation = transform.rotation;
-            float scale = 1 - (1.0f / 6) * i;
+            float scale = 1 - (1.0f / numOfParticles) * i;
             scale *= 0.5f;
             obj.transform.localScale = new Vector3(scale, scale, scale);
             Renderer rend = obj.GetComponentInChildren<Renderer>();
@@ -39,7 +45,7 @@ public class portalAnimation : MonoBehaviour {
 
     void Update() {
         if (timer < Time.time) {
-            duration = Random.Range(1, 2) * 0 + 1;
+            duration = Random.Range(1, 2) * 0 + timeBetweenPulses;
             timer = Time.time + duration;
             Pulse();
             print("pulse");
@@ -47,14 +53,14 @@ public class portalAnimation : MonoBehaviour {
 
         float anim =  1 - (timer - Time.time) / duration; // from 0 to 1
         foreach (GameObject child in portalFxs) {
-            child.transform.localPosition += transform.forward * child.transform.GetSiblingIndex() / 10000f;
-            child.transform.localScale += Vector3.one * child.transform.GetSiblingIndex() / 2500f;
+            child.transform.localPosition += transform.forward * child.transform.GetSiblingIndex() / 10000f * speedForwardMovement;
+            child.transform.localScale += Vector3.one * child.transform.GetSiblingIndex() / 2500f * speedScaling;
             Renderer rend = child.GetComponentInChildren<Renderer>();
             rend.material.color = new Color(
                 rend.material.color.r,
                 rend.material.color.g,
                 rend.material.color.b,
-                Mathf.Max(0, rend.material.color.a - 0.005f));
+                Mathf.Max(0, rend.material.color.a - 0.005f * speedTransparency));
         }
     }
 }
