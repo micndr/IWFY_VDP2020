7efd9980b5a75641cebd738e8a88bc062f7fa747
micndr <micndr@gmail.com>
2021-01-02 10:28:35 +0100

    disappearing script for notes

---
+++ IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/pauseMenuDisappear.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pauseMenuDisappear : MonoBehaviour
+{
+    public Image image;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            if (image.enabled)
+                image.enabled = false;
+        }
+    }
+}
