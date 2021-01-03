f2bb45c52179d309af66129b14bdcc844ab954ff
HomelessNoob <michele.zhu@mail.polimi.it>
2021-01-03 18:17:52 +0100

    Key_Letter_collected_Methods

---
+++ IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/Key_Letter_collected.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key_Letter_collected : MonoBehaviour
+{
+    void CanvasOn()
+    {
+        GetComponent<Canvas>().enabled = true;
+        StartCoroutine(CanvasOff());
+    }
+
+    private IEnumerator CanvasOff()
+    {
+        yield return new WaitForSeconds(3f);
+        GetComponent<Canvas>().enabled = false;
+    }
+}
