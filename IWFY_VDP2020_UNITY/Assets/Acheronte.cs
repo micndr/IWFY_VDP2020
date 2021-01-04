674e6def033759fd17db0f702857a7d2c43e784f
magiwanders <magi.wanders@gmail.com>
2021-01-04 02:54:30 +0100

    Lasciate ogni speranza voi che entrate

--- IWFY_VDP2020_UNITY/Assets/Acheronte.cs
+++ IWFY_VDP2020_UNITY/Assets/Acheronte.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Acheronte : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private GameObject [] objectsToSave;
+    [SerializeField] private Transform [] objectsToSave;
     void Start()
     {
         // TODO DESTROY ALL NON PUZZLE OBJECTS AND VAFFANCULO
-        Debug.Log("Caronte: DOV'È IL TUO OBOLO ANIMA MORTALE");
-        
+        Debug.Log("E tu che se' costì, anima viva, pàrtiti da cotesti che son morti.");
+        foreach (Transform child in transform) {
+            if (!objectsToSave.Contains(child))
+            {
+                Debug.Log("Guai a te anima prava!");
+                GameObject.Destroy(child.gameObject);
+            }
+            else Debug.Log("Vuolsi così cola dove si puote ciò che si vuole e di più non dimandare.");
+        }
     }
 }
