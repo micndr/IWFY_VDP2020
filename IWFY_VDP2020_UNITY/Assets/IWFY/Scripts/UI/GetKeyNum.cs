4e19d1da52acb1fc9959e299ba9e907ebf355039
jacopo grandi <jak.sk8@hotmail.it>
2021-01-04 16:26:28 +0100

    letter and keys visualization

---
+++ IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/UI/GetKeyNum.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GetKeyNum : MonoBehaviour {
+
+    TMPro.TextMeshProUGUI tmptext;
+    GlobalState gs;
+
+    void Update() {
+        if (!gs) gs = FindObjectOfType<GlobalState>();
+        if (!tmptext) tmptext = GetComponent<TMPro.TextMeshProUGUI>();
+        tmptext.text = "x0";
+        string lvl = SceneManager.GetActiveScene().name;
+        if (lvl.Substring(0, 5) == "World" && lvl != "WorldHub") {
+            int index = int.Parse(lvl[5].ToString()) - 1;
+            if (index < 4) {
+                tmptext.text = "x" + gs.keys[index].ToString();
+            }
+        }
+    }
+}
---
+++ IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/UI/GetLetterNum.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GetLetterNum : MonoBehaviour {
+
+    TMPro.TextMeshProUGUI tmptext;
+    GlobalState gs;
+
+    void Update() {
+        if (!gs) gs = FindObjectOfType<GlobalState>();
+        if (!tmptext) tmptext = GetComponent<TMPro.TextMeshProUGUI>();
+        tmptext.text = "x0";
+        string lvl = SceneManager.GetActiveScene().name;
+        if (lvl.Substring(0, 5) == "World") {
+            if (lvl == "WorldHub") {
+                // there are two letters in worldhub
+                tmptext.text = "x" + (gs.letters[0] + gs.letters[6]).ToString();
+            } else {
+                int index = int.Parse(lvl[5].ToString());
+                tmptext.text = "x" + gs.letters[index].ToString();
+            }
+        } 
+    }
+}
