d6fdccf8c67a931e10f9373ab9f519ee89adb4cb
jacopo grandi <jak.sk8@hotmail.it>
2021-01-04 16:03:57 +0100

    letter and keys progression

--- IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/Quest/ItemPickup.cs
+++ IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/Quest/ItemPickup.cs
@@ -7,6 +7,8 @@ public class ItemPickup : MonoBehaviour {
     public QuestMain main;
     public List<ItemObject> pickups = new List<ItemObject>();
 
+    public int keyNum = -1;
+
     void Start() {
         main = GameObject.Find("QuestMain").GetComponent<QuestMain>();
     }
@@ -15,6 +17,11 @@ public class ItemPickup : MonoBehaviour {
     public void GetItems () {
         foreach (ItemObject item in pickups) {
             main.inventory.AddItem(item, 1, item.itemID, item.itemName);
+        }
+
+        if (keyNum != -1) {
+            GlobalState gs = FindObjectOfType<GlobalState>();
+            gs.keys[keyNum] = 1;
         }
     }
 }
