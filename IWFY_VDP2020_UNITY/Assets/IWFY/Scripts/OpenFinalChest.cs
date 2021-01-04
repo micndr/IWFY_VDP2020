c4ff90b9129bfdea892331a665b1612afd89a248
jacopo grandi <jak.sk8@hotmail.it>
2021-01-04 17:43:01 +0100

    chest anim and progression

---
+++ IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/OpenFinalChest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenFinalChest : MonoBehaviour {
+
+    public Animator anim;
+    public GameObject collider;
+
+    void Trigger() {
+        GlobalState gs = FindObjectOfType<GlobalState>();
+        if (gs.completedQuests.Contains("Chest")) { return; }
+        int sum = 0; foreach (int n in gs.keys) sum += n;
+        if (sum == 4) {
+            anim.SetTrigger("trigger");
+            collider.SetActive(false);
+            gs.completedQuests.Add("Chest");
+        }
+    }
+}
