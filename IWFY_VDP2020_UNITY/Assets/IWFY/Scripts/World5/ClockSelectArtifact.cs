42f4322d47e59a778b065751fbdc02dc0763a55c
magiwanders <magi.wanders@gmail.com>
2021-01-06 19:11:24 +0100

    Fix mondo 5 pointer MALE MALE

--- IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/World5/ClockSelectArtifact.cs
+++ IWFY_VDP2020_UNITY/Assets/IWFY/Scripts/World5/ClockSelectArtifact.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,8 +7,15 @@ public class ClockSelectArtifact : MonoBehaviour {
 
     public bool OpenSelection = false;
     public int value = 0;
+    private GameObject _pointer;
+
+    private void Start()
+    {
+        _pointer = GameObject.FindGameObjectWithTag("IwfyReticlePointer");
+    }
 
     void OnPointerClick () {
         transform.parent.GetComponentInChildren<ClockSelect>().Select(gameObject);
+        _pointer.SendMessage("OnClickableObjectExit", SendMessageOptions.DontRequireReceiver);
     }
 }
